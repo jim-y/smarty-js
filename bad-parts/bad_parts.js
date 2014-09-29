a4ca001b94e8550a41b13364a92dae051357b4d5
Attila Kling <jimmyfly10@gmail.com>
2014-09-29 23:06:56 +0200

    Update bad_parts.js

--- bad-parts/bad_parts.js
+++ bad-parts/bad_parts.js
@@ -2,7 +2,10 @@
  * In JavaScript there's automatic semicolon insertion, that is, you are not forced
  * to use semicolons, because they will be inserted automatically. Hence, omitting
  * them could lead to hard-to-track errors. semicolonInsertionBAD shows the most
- * popular scenario where something could go bad.
+ * popular scenario where something could go bad. Th error
+ * //     logMe: function(obj) {
+ * //                    ^
+ * // SyntaxError: Unexpected token (
  * @author jim-y
  * @reading http://www.ecma-international.org/ecma-262/5.1/#sec-7.9.1
  * @return {Object} an object literal is inteded to be returned, but... read on ;)
@@ -16,13 +19,6 @@ function semicolonInsertionBAD() {
   }
 }
 
-// Test
-var log = semicolonInsertionBAD();
-log.logMe('I won\'t logged'); 
-//     logMe: function(obj) {
-//                    ^
-// SyntaxError: Unexpected token (
-
 function semicolonInsertionGOOD(argument) {
   return {
     logMe: function(obj) {
@@ -30,6 +26,3 @@ function semicolonInsertionGOOD(argument) {
     }
   };
 }
-
-var log = semicolonInsertionGOOD();
-log.logMe('I will be printed to the console');
