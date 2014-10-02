fcfaf20bb66f30a584952eb0b34de2d6ec417bf1
Kling Attila <jim-y@users.noreply.github.com>
2014-10-02 10:23:28 +0200

    Update good_to_know.js

--- good-to-know/good_to_know.js
+++ good-to-know/good_to_know.js
@@ -14,3 +14,16 @@ function objectPropsIssue() {
 module.exports = {
   objectPropsIssue: objectPropsIssue
 };
+
+/**
+ * I would assume that the return value will always be boolean, since && means, if leftOp AND rightOp are
+ * both true, then return true, otherwise false. In JavaScript its not how && works!!!
+ * The problem is, that the semantics are defined as ~ : if leftOp is true, then return rightOp
+ * which can lead to unwanted errors. BAD sample, DON'T do this!
+ * @author jim-y
+ * @reading http://www.ecma-international.org/ecma-262/5.1/#sec-11.11
+ * @return {(undefined|number)} !!!
+ */
+function easyToUseBadLogicalAND(arr) {
+  return arr && arr.length;
+}
