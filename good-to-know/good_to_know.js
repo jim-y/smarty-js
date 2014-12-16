1898c32a76e0c2ac3fe67eb079520ba3f1a3c251
Jim-Y <attila.kling@gmail.com>
2014-12-16 09:05:03 +0100

    Updated good-to-know

--- good-to-know/good_to_know.js
+++ good-to-know/good_to_know.js
@@ -24,6 +24,32 @@ function easyToUseBadLogicalAND(arr) {
   return arr && arr.length;
 }
 
+/**
+ * Dummy example to demonstrate the nearly useless comma operator.
+ * The comma operator's operands are expressions, and the values is as follows:
+ * "Evaluates the lefthand expression, then the righthand expression is
+ * evaluated and that is the result."
+ * @author jim-y
+ * @reading http://www.i-programmer.info/programming/javascript/6524-the-confusing-comma-in-javascript.html
+ * @return {String}
+ */
+function theCommaOperator(one) {
+  return (one = +!![]), 'I can write numbers like, '+ one + ', ' + ++one + ', ' + ++one + '.';
+}
+
+/**
+ * Dummy example 2 for comma op. Used in a for loop to initialize multiple 
+ * expressions. a.k.a batch the length of the array in a loop
+ * @author jim-y
+ * @reading https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Comma_Operator
+ */
+function theCommaOperator2(arr) {
+  var i, j, len;
+  for (i = 0, j = 0, len = arr.length; i < len; ++i, j += 2) {
+    console.log(arr[i] - (j - i));
+  };
+}
+
 // #################################
 module.exports = {
   objectPropsIssue: objectPropsIssue
